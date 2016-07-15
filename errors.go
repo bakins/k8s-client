dbda71c5ca0504519b04e29ec8afbf1fa0823c17
Brian Akins <bakins@yikyakapp.com>
2016-07-15 09:23:30 -0400

    Add Status type and use it as error

--- errors.go
+++ errors.go
@@ -2,23 +2,8 @@ package client
 
 import "github.com/pkg/errors"
 
-type (
-	notFoundError struct {
-		message string
-	}
-)
-
-// NewNotFoundError creates a new error. Generally only used by client implementations
-func NewNotFoundError(message string) error {
-	return &notFoundError{message: message}
-}
-
-func (n *notFoundError) Error() string {
-	return "object not found: " + n.message
-}
-
 // IsNotFoundError can be used to check if the error was a not found error.
 func IsNotFoundError(err error) bool {
-	_, ok := errors.Cause(err).(*notFoundError)
-	return ok
+	s, ok := errors.Cause(err).(*Status)
+	return ok && s.Code == 404
 }
